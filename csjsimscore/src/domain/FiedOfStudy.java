6e04e9af075c343fae007e534cf2030d49894cec
BiniyamTehakele <biniyamtehakele9@gmail.com>
2025-04-11 16:06:56 +0300

    FIRST DATABASE COMMIT

---
+++ csjsimscore/src/domain/FiedOfStudy.java
@@ -0,0 +1,31 @@
+package domain;
+public class FiedOfStudy {
+    int id;
+    String name;
+    int departmentId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+    
+}
